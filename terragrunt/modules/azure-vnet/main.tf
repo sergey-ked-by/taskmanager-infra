2468b4b3f857000b5fc6d21bf7a995fbda9e2120
Sergey Ked <sergey.ked.by@gmail.com>
2025-10-16 12:32:24 +0300

    fix: Add explicit dependency on resource group

--- terragrunt/modules/azure-vnet/main.tf
+++ terragrunt/modules/azure-vnet/main.tf
@@ -12,13 +12,13 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = [var.address_space]
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Create a subnet for Kubernetes (AKS)
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.aks_subnet_address_prefix]
 }
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "aks_subnet" {
 # Create a subnet for the PostgreSQL database
 resource "azurerm_subnet" "db_subnet" {
   name                 = "db-subnet"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.db_subnet_address_prefix]
 
@@ -45,13 +45,13 @@ resource "azurerm_subnet" "db_subnet" {
 # Create a Private DNS Zone for PostgreSQL
 resource "azurerm_private_dns_zone" "postgres_dns_zone" {
   name                = "${var.vnet_name}.private.postgres.database.azure.com"
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Link the Private DNS Zone to the Virtual Network
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   name                  = "${var.vnet_name}-postgres-dns-link"
-  resource_group_name   = var.resource_group_name
+  resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.postgres_dns_zone.name
   virtual_network_id    = azurerm_virtual_network.vnet.id
 }
