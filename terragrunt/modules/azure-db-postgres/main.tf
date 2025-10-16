5c3af5d94cac659dfd0be2f64db5c07e501faba6
Sergey Ked <sergey.ked.by@gmail.com>
2025-10-16 11:28:01 +0300

    fix: Remove hardcoded availability zone for postgres

--- terragrunt/modules/azure-db-postgres/main.tf
+++ terragrunt/modules/azure-db-postgres/main.tf
@@ -23,8 +23,6 @@ resource "azurerm_postgresql_flexible_server" "postgres_server" {
 
   sku_name   = "B_Standard_B1ms" # The smallest and cheapest SKU
   storage_mb = 32768             # 32 GB
-
-  zone = "1" # Place in the first availability zone
 }
 
 # Create a database within the server
